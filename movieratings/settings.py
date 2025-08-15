505b463c2a4bfe94b439079c55ed82d04ec8a972
Rishiraj Awasthi <142723698+Rishi730@users.noreply.github.com>
2025-08-15 16:58:03 +0530

    Update settings.py

--- movieratings/settings.py
+++ movieratings/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect after login
+LOGIN_REDIRECT_URL = '/'  # or any named URL in your app
